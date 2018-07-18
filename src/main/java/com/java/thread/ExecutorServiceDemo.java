7ad3c63046c979c0510ea6535d1029bb24dce8db
fyhailin <fyhailin@fyhailindeMacBook-Air.local>
2018-07-18 22:00:56 +0800

    增加线程池测试

---
+++ src/main/java/com/java/thread/ExecutorServiceDemo.java
@@ -0,0 +1,11 @@
+package com.java.thread;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ExecutorServiceDemo {
+
+	public static void main(String[] args) {
+		ExecutorService service =Executors.newCachedThreadPool();
+	}
+}
